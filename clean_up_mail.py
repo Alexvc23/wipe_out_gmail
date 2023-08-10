d0edeb7cf9dafd99f51587c8ef16ce922fdbd4b8
Alexvc23 <alexandervalencia1994@gmail.com>
2023-08-10 08:22:32 +0000

    project finished

---
+++ clean_up_mail.py
@@ -0,0 +1,66 @@
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import sys
+
+# If modifying these SCOPES, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.full']
+
+def get_service():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+    return service
+
+def delete_unread_emails(service):
+    user_id = 'me'
+    query = 'is:unread'
+    results = service.users().messages().list(userId=user_id, q=query).execute()
+    messages = results.get('messages', [])
+
+    for message in messages:
+        msg_id = message['id']
+        msg = service.users().messages().get(userId=user_id, id=msg_id).execute()
+        subject = [header['value'] for header in msg['payload']['headers'] if header['name'] == 'Subject'][0]
+        service.users().messages().delete(userId=user_id, id=msg_id).execute()
+        print(f'Deleted message with subject: {subject}')
+
+def delete_spam_emails(service):
+    user_id = 'me'
+    query = 'in:spam'
+    results = service.users().messages().list(userId=user_id, q=query).execute()
+    messages = results.get('messages', [])
+
+    for message in messages:
+        msg_id = message['id']
+        service.users().messages().delete(userId=user_id, id=msg_id).execute()
+        print(f'Message with ID {msg_id} deleted from Spam.')
+
+if __name__ == '__main__':
+    service = get_service()
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'delete_unread':
+            delete_unread_emails(service)
+        elif sys.argv[1] == 'delete_spam':
+            delete_spam_emails(service)
+        else:
+            print(f"Unknown argument: {sys.argv[1]}")
+            print("Usage: python script_name.py [delete_unread|delete_spam]")
+    else:
+        print("Specify an action: delete_unread or delete_spam")
---
+++ readme.md
@@ -0,0 +1,62 @@
+# Gmail Cleanup 🚀📧
+
+Tired of those unread emails or the growing spam messages? This Gmail Cleanup script is here to help you declutter your inbox in a flash! With simple commands, keep your Gmail neat and organized.
+
+## Table of Contents 📖
+
+- [Features](#features-✨)
+- [Setup](#setup-🛠)
+- [Usage](#usage-💻)
+- [Contributions](#contributions-🙌)
+- [License](#license-📜)
+
+## Features ✨
+
+1. **Delete Unread Emails**: Clears all unread emails with a single command.
+2. **Delete Spam**: Instantly clean out all messages in the spam folder.
+
+## Setup 🛠
+
+1. **Google Cloud Console Setup**: 
+   - Go to [Google Cloud Console](https://console.cloud.google.com/)
+   - Create a new project.
+   - Enable Gmail API.
+   - Create OAuth 2.0 credentials and download the `credentials.json` file. Place it in the project directory.
+
+2. **Environment Setup**:
+   - It's recommended to use a virtual environment:
+     ```
+     python3 -m venv myenv
+     source myenv/bin/activate
+     ```
+   - Install the required packages:
+     ```
+     pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+     ```
+
+3. **First-time Run**: 
+   - On first run, the script will open a new browser window asking for Gmail permissions.
+   - Authenticate and allow the script access.
+   - This will generate a `token.pickle` file which will be used for subsequent authentications.
+
+## Usage 💻
+
+To delete unread emails:
+```bash
+python3 script_name.py delete_unread
+```
+
+To delete emails from the spam folder:
+
+```bash
+python3 script_name.py delete_spam
+```
+
+Replace script_name.py with the name of your script file.
+
+## Contributions 🙌
+
+All contributions are welcome! If you have suggestions or improvements, please open an issue or send a pull request.
+License 📜
+
+This project is open-source, available under the [MIT License](https://choosealicense.com/licenses/mit/).
